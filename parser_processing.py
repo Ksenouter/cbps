2a74a149ee7c09fd9f9d264f5ed959b126f5b1b2
Prohorov Ilya <ProhorovIlyaDm@gmail.com>
2021-02-02 10:39:23 +0300

    Deleted form 135 settings

--- parser_processing.py
+++ parser_processing.py
@@ -9,8 +9,3 @@ def processing_form_101(dataframe):
 def processing_form_102(dataframe):
     # Place to write code
     return dataframe
-
-
-def processing_form_135(dataframe):
-    # Place to write code
-    return dataframe
